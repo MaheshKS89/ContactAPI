82e10569b83825133800f8371da33eeace73221b
Mahesh K S <mks.maheshks@outlook.com>
2024-09-23 23:09:26 +0530

    Add project files.

---
+++ ContactAPI/Data/ContactAPIDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ContactAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactAPI.Data
+{
+    public class ContactAPIDbContext : DbContext
+    {
+        public ContactAPIDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+
+    }
+}
